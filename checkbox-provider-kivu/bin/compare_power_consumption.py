ad9b7e31087825b16dbd368c7b06e60d90d61fa9
hector <122458375+petitfou@users.noreply.github.com>
2023-06-22 20:55:28 +0200

    Fix power usage comparison script (#63)
    Do not compute percentage as average percentages of separate components
    Should compute the overall power usage and compute gain from this value

--- checkbox-provider-kivu/bin/compare_power_consumption.py
+++ checkbox-provider-kivu/bin/compare_power_consumption.py
@@ -11,7 +11,15 @@ time_str = f'({p_float}) seconds time elapsed'
 energy_pattern = re.compile(energy_str)
 time_pattern = re.compile(time_str)
 
-def load_energy(fname: str ) -> tuple[float, dict[str, float]]:
+def load_energy(fname: str) -> tuple[float, dict[str, float]]:
+    """
+    Load file output by rapl-power-stat.py
+    The return value is a 2-tuple of time (sec) and dict
+      The dict:
+        - key : power zone name
+        - value ; joules values
+      Example: (600.097183, {'psys': 9976.218114, 'package-0': 3658.829745, 'core': 825.515099, 'uncore': 318.009806})
+    """
     energy_usage = {}
     duration = 0.0
     with open(fname, 'r') as f:
@@ -26,6 +34,15 @@ def load_energy(fname: str ) -> tuple[float, dict[str, float]]:
 
     return (duration, energy_usage)
 
+def get_energy_usage(rapl_output):
+    """
+    Return total energy usage according to a RAPL output
+    """
+    consumed_energy = rapl_output.get('package-0', 0.0)
+    # on some recent architectures, we have psys package that includes package-0
+    consumed_energy = max(consumed_energy, rapl_output.get('psys', 0.0))
+    return consumed_energy
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         sys.exit(1)
@@ -36,25 +53,29 @@ if __name__ == "__main__":
 
     hwacc_energy = load_energy(hwacc_energy_file)
     no_hwacc_energy = load_energy(no_hwacc_energy_file)
-    
-    print(f'Enabled: {hwacc_energy}')
-    print(f'Disabled: {no_hwacc_energy}')
 
-    gain_energy = {}    
-    for key, value in hwacc_energy[1].items():
-        hwacc_joule_per_sec = value / hwacc_energy[0]
-        no_hwacc_joule_per_sec = no_hwacc_energy[1][key] / no_hwacc_energy[0]
-        if no_hwacc_joule_per_sec != 0:
-            gain = ((no_hwacc_joule_per_sec - hwacc_joule_per_sec) / no_hwacc_joule_per_sec) * 100
-        else:
-            gain = -1000000 * hwacc_joule_per_sec
-        gain_energy[key] = gain
+    hwacc_cons = get_energy_usage(hwacc_energy[1])
+    no_hwacc_cons = get_energy_usage(no_hwacc_energy[1])
 
-    avg_gain = statistics.mean(gain_energy.values())
+    print(f'Enabled: {hwacc_energy} - Consumption : {hwacc_cons}')
+    print(f'Disabled: {no_hwacc_energy} - Consumption : {no_hwacc_cons}')
 
-    print((avg_gain, gain_energy))
+    hwacc_duration_sec = hwacc_energy[0]
+    no_hwacc_duration_sec = no_hwacc_energy[0]
+    if (hwacc_duration_sec <= 0) or (no_hwacc_duration_sec <= 0):
+        sys.exit(1)
 
-    if avg_gain < min_gain:
+    hwacc_joule_per_sec = hwacc_cons / hwacc_duration_sec
+    no_hwacc_joule_per_sec = no_hwacc_cons / no_hwacc_duration_sec
+
+    if no_hwacc_joule_per_sec <= 0:
+        sys.exit(1)
+
+    gain = ((no_hwacc_joule_per_sec - hwacc_joule_per_sec) / no_hwacc_joule_per_sec) * 100
+
+    print(f'Gain : {gain}%')
+
+    if gain < min_gain:
         sys.exit(1)
 
 sys.exit(0)
